62a364246164fc7045b73a8e1d7db04f1abecf56
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 21:17:31 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
-#include<iostream>
+#include <iostream>
+#include <ctime> 
 
-int main() {
+    int main() {
+        const int rows = 3;
+        const int cols = 4;
 
-	return 0
-}
+        int arr[rows][cols];
+
+
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+
+        std::cout << "Randomly generated array:" << std::endl;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = std::rand() % 100;
+                std::cout << arr[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        int sum = 0;
+        int minElement = arr[0][0];
+        int maxElement = arr[0][0];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum += arr[i][j];
+                if (arr[i][j] < minElement) {
+                    minElement = arr[i][j];
+                }
+                if (arr[i][j] > maxElement) {
+                    maxElement = arr[i][j];
+                }
+            }
+        }
+
+        double average = static_cast<double>(sum) / (rows * cols);
+
+        std::cout << "Sum of elements: " << sum << std::endl;
+        std::cout << "Average: " << average << std::endl;
+        std::cout << "Minimum element: " << minElement << std::endl;
+        std::cout << "Maximum element: " << maxElement << std::endl;
+
+        return 0;
+    }
