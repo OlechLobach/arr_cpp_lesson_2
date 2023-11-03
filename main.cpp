1c9570ae78b78e86a4dc254e887c3a1cea189c46
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 21:23:34 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
-#include<iostream>
+#include <iostream>
+
+
 
 int main() {
+    const int rows1 = 5;
+    const int cols1 = 10;
+    const int rows2 = 5;
+    const int cols2 = 5;
 
-	return 0
+    int arr1[rows1][cols1];
+    int arr2[rows2][cols2];
+
+
+    srand(time(0));
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            arr1[i][j] = rand() % 51;
+        }
+    }
+
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            arr2[i][j] = arr1[i][2 * j] + arr1[i][2 * j + 1];
+        }
+    }
+
+    std::cout << "First Array:" << std::endl;
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            std::cout << arr1[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Second Array:" << std::endl;
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            std::cout << arr2[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
