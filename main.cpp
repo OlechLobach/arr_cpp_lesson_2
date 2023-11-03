8b65416616561e272e6e3c95eb295c05d3a36e77
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 22:23:39 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 
 int main() {
+    int n;
+    std::cout << "Enter the initial number: ";
+    std::cin >> n;
 
-	return 0
+    const int rows = 5; 
+    const int cols = 5;
+
+    int arr[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = n;
+            n += 1;
+        }
+    }
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+	return 0;
 }
