7f0c34d9497e4aac7145dba291f4bc745c7c89ae
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-03 22:16:23 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
+#include <iostream>
 
 int main() {
+    int n;
+    std::cout << "Enter the initial number: ";
+    std::cin >> n;
 
-	return 0
+    const int rows = 3; 
+    const int cols = 3;
+
+    int arr[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = n;
+            n *= 2;
+        }
+    }
+
+ 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            //не зрозумів у яку сторону повинні писатися цифри за збільшенням, с права на ліво чи з верху вниз
+            //генерація цифр з верху в низ
+            std::cout << arr[j][i] << " "; 
+
+
+            //генерація цифр з права на ліво
+               /* std::cout << arr[i][j] << " ";*/
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
